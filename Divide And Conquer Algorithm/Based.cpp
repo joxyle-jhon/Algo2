31053a24c11b59fb34a2936c0082b0da1b4f8e44
Jhon Lloyd Omblero <omblero.jhonlloyd.04@gmail.com>
2024-04-02 22:59:51 +0800

    Added

---
+++ Divide And Conquer Algorithm/Based.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Function to compute a^b using Divide-and-Conquer
+long long power(int a, int b) {
+    if (b == 0) return 1; // Base case: a^0 = 1
+    long long half_power = power(a, b / 2); // Divide
+    if (b % 2 == 0)
+        return half_power * half_power; // Combine for even exponent
+    else
+        return a * half_power * half_power; // Combine for odd exponent
+}
+
+int main() {
+    int a = 3, b = 5;
+    std::cout << "Compute " << a << "^" << b << std::endl;
+    std::cout << "Result: " << power(a, b) << std::endl;
+    return 0;
+}
+
+
